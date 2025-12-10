8df2ba5d9bdf833d6a3b3da0fd65f73d495db496
d0nghye0n <c20537@kma.ac.kr>
2025-12-10 02:38:01 +0000

    124

--- sample/app/__init__.py
+++ sample/app/__init__.py
@@ -8,7 +8,7 @@ from werkzeug.middleware.proxy_fix import ProxyFix
 
 import os
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static', template_folder='templates')
 
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
